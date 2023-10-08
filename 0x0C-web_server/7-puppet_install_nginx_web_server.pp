1f2eaa3de99acb18ca5209de13a8be68fd9aa122
Nidhal-karmadi <nidhalwhite@gmail.com>
2023-10-08 06:34:13 -0700

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,14 @@
-# Puppet manifest to install nginx
+# Automating project requirements using Puppet
+
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'install':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Nidhal-karmadi permanent;',
 }
 
 file { '/var/www/html/index.html':
